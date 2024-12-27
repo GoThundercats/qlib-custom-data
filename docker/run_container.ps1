4d9098cbda388e71252b995ca62895fb9b75c6eb
GoThundercats <murrayeddie@gmail.com>
2024-12-27 13:19:54 +0000

    Updated run script to use saved image with installations

--- docker/run_container.ps1
+++ docker/run_container.ps1
@@ -3,3 +3,17 @@ docker run -it `
     -v C:/Users/murra/qlib-data/qlib_data:/root/.qlib/qlib_data/my_data `
     -v C:/Users/murra/qlib-custom-data/scripts:/root/scripts `
     microsoft/qlib
+
+
+./docker/run_container.ps1
+
+
+# First build the image
+docker build -t qlib-custom -f docker/Dockerfile .
+
+# Then run with volumes
+docker run -it `
+    -v C:/Users/murra/qlib-data/csv_data:/root/.qlib/csv_data/my_data `
+    -v C:/Users/murra/qlib-data/qlib_data:/root/.qlib/qlib_data/my_data `
+    -v C:/Users/murra/qlib-custom-data/scripts:/root/scripts `
+    qlib-custom
