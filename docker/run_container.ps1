4800eea2b92e72de051e5ec58ec121fb20e055cc
GoThundercats <murrayeddie@gmail.com>
2024-12-27 12:59:10 +0000

    Added container run script with volume mappings

---
+++ docker/run_container.ps1
@@ -0,0 +1,5 @@
+docker run -it `
+    -v C:/Users/murra/qlib-data/csv_data:/root/.qlib/csv_data/my_data `
+    -v C:/Users/murra/qlib-data/qlib_data:/root/.qlib/qlib_data/my_data `
+    -v C:/Users/murra/qlib-custom-data/scripts:/root/scripts `
+    microsoft/qlib
